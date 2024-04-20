yuvrajsingsolanki/Tow-Boot

commit 39180168d4195ca4e50e983666ab01cde27dfa11

tow-boot/builder: pass config validation as a package
Otherwise the big script gets passed around as an argument and simply is
too big.

See also Mobile NixOS d21e6dad77de0383cbb17c78a5299fb691eb89a8

support/nix/eval-kconfig.nix
@@ -4,6 +4,7 @@
 , modules ? []
 , structuredConfig
 , version
+, writeShellScript
 }: rec {
   module = import (path + "/nixos/modules/system/boot/kernel_config.nix");
   config = (lib.evalModules {
@@ -42,7 +43,7 @@
           mkConf = cfg: lib.concatStringsSep "\n" (lib.mapAttrsToList mkConfigLine cfg);
           configfile = mkConf config.settings;
 
-          validatorSnippet = ''
+          validatorSnippet = writeShellScript "kernel-configuration-validator-snippet" ''
             (
             echo
             echo ":: Validating kernel configuration"
@@ -61,7 +62,6 @@
               line = lib.escapeShellArg (mkConfigLine key item);
               escapedLinePattern = lib.replaceStrings ["[" "]" ''\''] [ ''\['' ''\]'' ''\\''] line;
               lineNotSet = "# CONFIG_${key} is not set";
-              linePattern = "^CONFIG_${key}=";
               presencePattern = "CONFIG_${key}[ =]";
             in
             ''
@@ -150,9 +150,9 @@
             };
             validatorSnippet = lib.mkOption {
               readOnly = true;
-              type = lib.types.str;
+              type = lib.types.package;
               description = ''
-                String that can directly be used as a kernel config file contents.
+                Path to a script that can directly be called to validate the kernel config.
               '';
             };
           };

modules/tow-boot/builder.nix
@@ -15,7 +15,7 @@ let
 
   evaluatedStructuredConfig = import ../../support/nix/eval-kconfig.nix rec {
     inherit lib;
-    inherit (pkgs) path;
+    inherit (pkgs) path writeShellScript;
     version = config.Tow-Boot.uBootVersion;
     structuredConfig = (config.Tow-Boot.structuredConfigHelper version);
   };

commit b33c75e395f2b3d90f7406eaa7eaa047f451df46

support/eval-kconfig: Fix regression not halting build
Introduced by 39180168d4195ca4e50e983666ab01cde27dfa11

support/nix/eval-kconfig.nix
@@ -45,6 +45,10 @@
 
           validatorSnippet = writeShellScript "kernel-configuration-validator-snippet" ''
             (
+            # This can be executed outside of a Nix build script.
+            set -eu
+            set -o pipefail
+
             echo
             echo ":: Validating kernel configuration"
             echo